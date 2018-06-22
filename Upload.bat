642d1b2ad641916586a463ab944ab9496b5d781e
zkphyj <zkphyj@163.com>
2018-06-22 17:05:12 -0500

    Site updated: 2018/06/22 17:05:12

--- Upload.bat
+++ Upload.bat
@@ -4,4 +4,4 @@ xcopy /s/e/i/y .deploy_git\.git .git
 rd /s/q .deploy_git
 git add .
 git commit -m "Site updated: %date:~0,10% %time:~0,8%"
-git push "https://github.com/zkphyj/zkphyj.github.io.git" hexo:hexo
+git push "https://github.com/zkphyj/zkphyj.github.io.git" hexo:hexo  >blog.log
